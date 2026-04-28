0df207f85a8c17edab6efb2ae453e0a1a352d66e
kensylee95 <kingsleyumenze@gmail.com>
2026-04-28 18:03:41 +0100

    linked pages

--- apps/user-web/src/app/(auth)/event/view/[eventSlug]/page.tsx
+++ apps/user-web/src/app/(auth)/event/view/[eventSlug]/page.tsx
@@ -1,10 +1,8 @@
-import SplashScreen from "@/ui/Splash/Splash";
-
 interface PageProps {
   params: Promise<{ eventSlug: string }>;
 }
 export default async function Page({ params }: PageProps) {
   const { eventSlug } = await params;
   console.log(eventSlug);
-  return <SplashScreen />;
+  return <div></div>;
 }
